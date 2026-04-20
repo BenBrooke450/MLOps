fd1356265083bfe00addab0b674d0e191f2aa3f7
BenBrooke450 <bensir@mail.com>
2026-04-21 00:14:55 +0200

    UPDATE

--- Complete_MLOps_Bootcamp_With_10_End_To_End_ML_Projects/6.MLFLOW_with_AWS_cloud/app.py
+++ Complete_MLOps_Bootcamp_With_10_End_To_End_ML_Projects/6.MLFLOW_with_AWS_cloud/app.py
@@ -30,6 +30,40 @@ url = "https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/wi
 if __name__ == "__main__":
     pass
 
+    try:
+        data = pd.read_csv(url)
+    except:
+        logger.exception("Error, unable to download the data")
+
+    train , test = train_test_split(data)
+
+    X_train = train.drop('quality',axis=1)
+    X_test = test.drop('quality',axis=1)
+    y_train = train[['quality']]
+    y_test = test[['quality']]
+
+    alpha  = float(sys.argv[1]) if len(sys.argv)>1 else 0.5
+    l1_ratio = float(sys.argv[2]) if len(sys.argv)>2 else 0.5
+
+    with mlflow.start_run():
+        lr=ElasticNet(alpha=alpha,l1_ratio=l1_ratio,random_state=42)
+        lr.fit(X_train,y_train)
+
+        predict_qualities = lr.predict(X_test)
+        (rmse, mae, r2) = eval_metrics(y_test,predict_qualities)
+
+        mlflow.log_metric("alpha", alpha)
+        mlflow.log_metric("l1_ratio", l1_ratio)
+        mlflow.log_metric("rmse", rmse)
+        mlflow.log_metric("alpha", alpha)
+        mlflow.log_metric("mae", mae)
+
+
+
+
+
+
+
 
 
 
