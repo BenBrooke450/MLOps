85f28d6d83d3c49c3ecf2be63dce64a804dd203e
BenBrooke450 <bensir@mail.com>
2026-04-21 00:07:09 +0200

    UPDATE

---
+++ Complete_MLOps_Bootcamp_With_10_End_To_End_ML_Projects/6.MLFLOW_with_AWS_cloud/app.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import os
+import sys
+import numpy as np
+import sklearn
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import ElasticNet
+from urllib.parse import urlparse
+import mlflow
+from mlflow.models.signature import infer_signature
+import mlflow.sklearn
+import logging
+
+
+logging.basicConfig(level=logging.WARN)
+logger = logging.getLogger(__name__)
+
+
+
+def eval_metrics(actual,pred):
+    rmse = np.sqrt(mean_squared_error(actual,pred))
+    mae = mean_absolute_error(actual,pred)
+    r2 = r2_score(actual,pred)
+    return rmse,mae,r2
+
+
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-white.csv"
+
+if __name__ == "__main__":
+    pass
+
+
+
+
+
+
+
+
+
+
