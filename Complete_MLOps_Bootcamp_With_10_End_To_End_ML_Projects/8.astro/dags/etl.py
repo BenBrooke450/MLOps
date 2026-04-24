0c5f2437e4d55ec9ab2d7296cf67ca8e0c5cee46
BenBrooke450 <bensir@mail.com>
2026-04-24 23:50:34 +0200

    UPDATE

--- Complete_MLOps_Bootcamp_With_10_End_To_End_ML_Projects/8.astro/dags/etl.py
+++ Complete_MLOps_Bootcamp_With_10_End_To_End_ML_Projects/8.astro/dags/etl.py
@@ -1,84 +1,71 @@
 from airflow import DAG
-from airflow.providers.http.operators.http import HttpOperator
 from airflow.decorators import task
 from airflow.providers.postgres.hooks.postgres import PostgresHook
-import json
-
 from datetime import datetime
+import requests
 
 with DAG(
     dag_id="nasa_apod_postgres",
     start_date=datetime(2024, 1, 1),
     schedule="@daily",
-    catchup=False) as dag:
+    catchup=False
+) as dag:
 
+    # 1. Create table
     @task
     def create_table():
-        postgres_hook = PostgresHook(postgres_conn_id="nasa_apod")
-
-        create_table_query = """
-            CREATE TABLE IF NOT EXISTS apod_data (
+        hook = PostgresHook(postgres_conn_id="nasa_apod")
+        hook.run("""
+        CREATE TABLE IF NOT EXISTS apod_data (
             id SERIAL PRIMARY KEY,
-            title VARCHAR(255),
+            title TEXT,
             explanation TEXT,
             url TEXT,
             date DATE,
-            media_type varchar(50));"""
+            media_type TEXT
+        );
+        """)
 
-        postgres_hook.run(create_table_query)
 
+    @task
+    def extract_apod():
+        url = "https://api.nasa.gov/planetary/apod"
+        params = {
+            "api_key": "OdSlC3kbgxUPIfIMPN2oUSq7hkEaB62oWoafAxyF"
+        }
 
-
-    ## https://api.nasa.gov/planetary/apod?api_key=OdSlC3kbgxUPIfIMPN2oUSq7hkEaB62oWoafAxyF
-    key =  "OdSlC3kbgxUPIfIMPN2oUSq7hkEaB62oWoafAxyF"
-
-    extract_apod = HttpOperator(
-        task_id="extract_apod",
-        http_conn_id="nasa_api",
-        endpoint = "planetary/apod?api_key=OdSlC3kbgxUPIfIMPN2oUSq7hkEaB62oWoafAxyF",
-        method='GET',
-        response_filter=lambda response:response.json(),
-
-    )
+        response = requests.get(url, params=params)
+        return response.json()
 
 
     @task
     def transform_apod_data(response):
-        apod_data = {
-            "title": response.get("title",""),
-            "url": response.get("explanation",""),
-            "date": response.get("date",""),
-            "media_type": response.get("media_type",""),
-            "explanation": response.get("explanation","")
+        return {
+            "title": response.get("title", ""),
+            "url": response.get("url", ""),
+            "date": response.get("date", ""),
+            "media_type": response.get("media_type", ""),
+            "explanation": response.get("explanation", "")
         }
-
-        return apod_data
-
 
 
     @task
     def load_data_to_postgres(apod_data):
-        postgres_hook = PostgresHook(postgres_conn_id="nasa_apod")
+        hook = PostgresHook(postgres_conn_id="nasa_apod")
 
-        insert_query = """
+        hook.run("""
         INSERT INTO apod_data (title, url, date, media_type, explanation)
-        VALUES (%s, %s, %s, %s, %s);
-            """
-
-        postgres_hook.run(insert_query, parameters = (
-                          apod_data["title"],
-                          apod_data["explanation"],
-                          apod_data["url"],
-                          apod_data["date"],
-                          apod_data["media_type"]))
-
-        create_tbl = create_table()
-
-        transformed = transform_apod_data(extract_apod.output)
-
-        load_task = load_data_to_postgres(transformed)
-
-        create_tbl >> extract_apod >> transformed >> load_task
+        VALUES (%s, %s, %s, %s, %s)
+        """, parameters=(
+            apod_data["title"],
+            apod_data["url"],
+            apod_data["date"],
+            apod_data["media_type"],
+            apod_data["explanation"],
+        ))
 
 
-
+    table = create_table()
+    raw = extract_apod()
+    cleaned = transform_apod_data(raw)
+    load_data_to_postgres(cleaned)
