cd9e7bbe623c952e8b9aadaae8e9a20df8af37f9
BenBrooke450 <bensir@mail.com>
2026-04-30 00:05:24 +0200

    UPDATE

---
+++ Big_project/datascience/pipeline/data_validation.py
@@ -0,0 +1,26 @@
+from datascience.config.configuration import *
+from datascience import logger
+from datascience.components.data_validation import *
+
+STAGE_NAME = "data_validation"
+
+class DataValidationPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_validation_config = DataValidationConfig()
+        data_validation = DataValidation(config=data_validation_config)
+        data_validation.validate_all_columns()
+
+
+
+if __name__ == "__main__":
+    try:
+        logger.info(F"------- stage: {STAGE_NAME} STARTED-------")
+        obj = DataValidationPipeline()
+        obj.main()
+        logger.info(F"------- stage: {STAGE_NAME} FINISHED-------")
+    except Exception as e:
+        logger.error(e)
