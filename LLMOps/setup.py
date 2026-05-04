35abed8ef79c0b46fd3618ed16119457a32702a0
BenBrooke450 <bensir@mail.com>
2026-05-04 23:08:45 +0200

    UPDATE

--- LLMOps/setup.py
+++ LLMOps/setup.py
@@ -26,4 +26,25 @@ for epochs in range(10):
     mlflow.log_metric("accuracy", np.random.random(),step=epochs)
     mlflow.log_metric("loss", np.random.random(), step=epochs)
 
+mlflow.end_run()
+
+
+
+mlflow.set_experiment("My other experiment for LLM")
+
+with mlflow.start_run(run_name="sin"):
+    for t in range(100):
+        metric_value = np.sin(t * np.pi/50)
+        mlflow.log_metric("matric_value", metric_value, step = t)
+
+
+
+
+
+with mlflow.start_run(run_name="cos"):
+    for t in range(100):
+        metric_value = np.cos(t * np.pi / 50)
+        mlflow.log_metric("matric_value", metric_value, step=t)
+
+
 
