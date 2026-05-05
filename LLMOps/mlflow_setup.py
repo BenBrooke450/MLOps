29414ea161fd04edb90a390c38f40db61de46b2c
BenBrooke450 <bensir@mail.com>
2026-05-05 18:21:20 +0200

    UPDATE

---
+++ LLMOps/mlflow_setup.py
@@ -0,0 +1,50 @@
+import torch
+import mlflow
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+from datasets import load_dataset
+
+params = {
+    "model_name":"distilbert-base-uncased",
+    "learning_rate":0.001,
+    "batch_size":32,
+    "num_epochs":3,
+    "dataset_name":"ag_news",
+    "task_name":"sequence_classification",
+    "max_seq_length":128,
+    "output_dir":"models/distilbert_ag-news"
+    }
+
+mlflow.set_tracking_uri("http://localhost:5001")
+
+mlflow.set_experiment("distilbert-base-uncased")
+
+run = mlflow.start_run(run_name="distilbert-base-uncased")
+
+mlflow.log_params(params)
+
+
+
+
+dataset = load_dataset("imdb", data_dir="/Users/benjaminbrooke/PycharmProjects/MLOps/LLMOps")
+
+save_path = "/Users/benjaminbrooke/PycharmProjects/MLOps/LLMOps/flan-t5-small"
+
+model = AutoModelForSeq2SeqLM.from_pretrained("google/flan-t5-small")
+tokenizer = AutoTokenizer.from_pretrained("google/flan-t5-small")
+
+def tokenise(batch):
+    return tokenizer(batch["text"],padding="max_length",
+                     truncate=True,
+                     max_length=params["max_seq_length"])
+
+train_dataset = dataset["train"].shuffle()\
+                 .select(range(200))\
+                 .map(tokenise,batched=True)
+
+test_dataset = dataset["test"].shuffle()\
+                 .select(range(200))\
+                 .map(tokenise,batched=True)
+
+
+
+
--- LLMOps/setup.py
+++ LLMOps/setup.py
@@ -1,5 +1,9 @@
 import mlflow
 import numpy as np
+import pandas as pd
+from transformers import pipeline
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+
 
 
 mlflow.set_tracking_uri("http://localhost:5001")
@@ -45,6 +49,38 @@ with mlflow.start_run(run_name="cos"):
     for t in range(100):
         metric_value = np.cos(t * np.pi / 50)
         mlflow.log_metric("matric_value", metric_value, step=t)
+
+
+
+with mlflow.start_run(run_name="numbers"):
+    x = list(range(100))
+    y = list(range(1,200,2))
+
+    df = pd.DataFrame({"x": x, "y": y})
+
+
+    with open("/Users/benjaminbrooke/PycharmProjects/MLOps/LLMOps/data.csv", "w") as f:
+        df.to_csv(f, index=False)
+
+    mlflow.log_artifact("/Users/benjaminbrooke/PycharmProjects/MLOps/LLMOps/data.csv")
+
+
+
+
+
+
+save_path = "/Users/benjaminbrooke/PycharmProjects/MLOps/LLMOps/flan-t5-small"
+
+model = AutoModelForSeq2SeqLM.from_pretrained("google/flan-t5-small")
+tokenizer = AutoTokenizer.from_pretrained("google/flan-t5-small")
+
+model.save_pretrained(save_path)
+tokenizer.save_pretrained(save_path)
+
+
+
+with mlflow.start_run(run_name="model"):
+    mlflow.log_artifact("/Users/benjaminbrooke/PycharmProjects/MLOps/LLMOps/flan-t5-small")
 
 
 
